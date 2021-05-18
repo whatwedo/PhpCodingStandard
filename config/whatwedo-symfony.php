9571d0ba3f22e0e50536cfe53b929b7c28c78f9f
Marc Wieland <marc@whatwedo.ch>
2021-05-18 17:13:41 +0100

    feat: update config files

---
+++ config/whatwedo-symfony.php
@@ -0,0 +1,20 @@
+<?php
+
+declare(strict_types=1);
+
+use PhpCsFixerCustomFixers\Fixer\NoDoctrineMigrationsGeneratedCommentFixer;
+use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
+
+return static function (ContainerConfigurator $containerConfigurator): void {
+    $containerConfigurator->import(__DIR__ . '/../../../symplify/easy-coding-standard/config/set/symfony.php');
+
+    $containerConfigurator->import(__DIR__ . '/whatwedo-common.php');
+
+    $services = $containerConfigurator->services();
+
+    $services->set(NoDoctrineMigrationsGeneratedCommentFixer::class);
+
+    $parameters = $containerConfigurator->parameters();
+
+    $parameters->set('skip', ['Symplify\CodingStandard\Sniffs\Naming\AbstractClassNameSniff' => ['**/Entity/*.php']]);
+};
