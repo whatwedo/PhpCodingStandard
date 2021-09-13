87b99460cd6ff189e97de37658d66654484da87f
Marc Wieland <marc@whatwedo.ch>
2021-09-13 16:11:37 +0200

    feat: skip some rules for commenting (WordPress specific)

--- config/whatwedo-wordpress.php
+++ config/whatwedo-wordpress.php
@@ -3,7 +3,16 @@
 declare(strict_types=1);
 
 use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
+use Symplify\EasyCodingStandard\ValueObject\Option;
 
 return static function (ContainerConfigurator $containerConfigurator): void {
     $containerConfigurator->import(__DIR__ . '/whatwedo-common.php');
+    $parameters = $containerConfigurator->parameters();
+
+    $parameters->set(Option::SKIP, [
+        PHP_CodeSniffer\Standards\Squiz\Sniffs\Commenting\ClassCommentSniff::class . '.Missing' => null,
+        PHP_CodeSniffer\Standards\Squiz\Sniffs\Commenting\FileCommentSniff::class . '.Missing' => null,
+        PHP_CodeSniffer\Standards\Squiz\Sniffs\Commenting\FileCommentSniff::class . '.MissingPackageTag' => null,
+        PHP_CodeSniffer\Standards\Squiz\Sniffs\Commenting\FileCommentSniff::class . '.WrongStyle' => null,
+    ]);
 };
