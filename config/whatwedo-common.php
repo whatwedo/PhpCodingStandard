d618832af7cbe99d52d871b6ee75219264e2bf63
Marc Wieland <marc@whatwedo.ch>
2022-12-05 19:44:01 +0400

    feat: update config to new syntax

--- config/whatwedo-common.php
+++ config/whatwedo-common.php
@@ -2,6 +2,7 @@
 
 declare(strict_types=1);
 
+use Symplify\EasyCodingStandard\Config\ECSConfig;
 use PHP_CodeSniffer\Standards\Generic\Sniffs\CodeAnalysis\AssignmentInConditionSniff;
 use PHP_CodeSniffer\Standards\Generic\Sniffs\ControlStructures\InlineControlStructureSniff;
 use PHP_CodeSniffer\Standards\Generic\Sniffs\WhiteSpace\ScopeIndentSniff;
@@ -36,7 +37,6 @@ use PhpCsFixer\Fixer\Strict\StrictParamFixer;
 use PhpCsFixer\Fixer\Whitespace\BlankLineBeforeStatementFixer;
 use PhpCsFixer\Fixer\Whitespace\MethodChainingIndentationFixer;
 use PhpCsFixer\Fixer\Whitespace\NoExtraBlankLinesFixer;
-use PhpCsFixer\Fixer\Whitespace\NoExtraConsecutiveBlankLinesFixer;
 use PhpCsFixerCustomFixers\Fixer\NoImportFromGlobalNamespaceFixer;
 use PhpCsFixerCustomFixers\Fixer\NoNullableBooleanTypeFixer;
 use PhpCsFixerCustomFixers\Fixer\NoPhpStormGeneratedCommentFixer;
@@ -48,112 +48,93 @@ use SlevomatCodingStandard\Sniffs\Exceptions\DeadCatchSniff;
 use SlevomatCodingStandard\Sniffs\Namespaces\ReferenceUsedNamesOnlySniff;
 use SlevomatCodingStandard\Sniffs\Namespaces\UseFromSameNamespaceSniff;
 use SlevomatCodingStandard\Sniffs\Variables\UselessVariableSniff;
-use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
 use Symplify\EasyCodingStandard\ValueObject\Option;
 use Symplify\EasyCodingStandard\ValueObject\Set\SetList;
 
-return static function (ContainerConfigurator $containerConfigurator): void {
-    $containerConfigurator->import(SetList::CLEAN_CODE);
-    $containerConfigurator->import(SetList::COMMON);
-    $containerConfigurator->import(SetList::PSR_12);
+return static function (ECSConfig $ecsConfig): void {
+    $ecsConfig->sets([SetList::CLEAN_CODE]);
+    $ecsConfig->sets([SetList::COMMON]);
+    $ecsConfig->sets([SetList::PSR_12]);
 
-    $services = $containerConfigurator->services();
-
-    $services->set(ValidClassNameSniff::class);
-
-    $services->set(ClassCommentSniff::class);
-
-    $services->set(FileCommentSniff::class);
-
-    $services->set(FunctionCommentThrowTagSniff::class);
-
-    $services->set(OrderedClassElementsFixer::class)
-        ->call('configure', [
-            [
-                'order' => [
-                    'use_trait',
-                    'constant_public',
-                    'constant_protected',
-                    'constant_private',
-                    'property_public',
-                    'property_protected',
-                    'property_private',
-                    'construct',
-                    'destruct',
-                    'method_public',
-                    'method_protected',
-                    'method_private',
-                    'phpunit',
-                    'magic',
-                    'magic',
-                ],
-            ], ]);
-
-    $services->set(NoExtraConsecutiveBlankLinesFixer::class);
-
-    $services->set(NoImportFromGlobalNamespaceFixer::class);
-
-    $services->set(NoNullableBooleanTypeFixer::class);
-
-    $services->set(NoPhpStormGeneratedCommentFixer::class);
-
-    $services->set(PhpdocSelfAccessorFixer::class);
-
-    $services->set(PhpdocVarAnnotationCorrectOrderFixer::class);
-
-    $services->set(ForbiddenAnnotationsSniff::class);
-
-    $services->set(AssignmentInConditionSniff::class);
-
-    $services->set(DeadCatchSniff::class);
-
-    $services->set(UseFromSameNamespaceSniff::class);
-
-    $parameters = $containerConfigurator->parameters();
-
-    $parameters->set(Option::PARALLEL, true);
-
-    $parameters->set(Option::SKIP, [
-        AssignmentInConditionSniff::class => null,
-        InlineControlStructureSniff::class => null,
-        ScopeIndentSniff::class => null,
-        ScopeClosingBraceSniff::class => null,
-        CastSpacesFixer::class => null,
-        NoTrailingCommaInListCallFixer::class => null,
-        YodaStyleFixer::class => null,
-        IsNullFixer::class => null,
-        BlankLineAfterNamespaceFixer::class => null,
-        BinaryOperatorSpacesFixer::class => null,
-        ConcatSpaceFixer::class => null,
-        IncrementStyleFixer::class => null,
-        NotOperatorWithSuccessorSpaceFixer::class => null,
-        UnaryOperatorSpacesFixer::class => null,
-        PhpCsFixer\Fixer\Phpdoc\BlankLineBeforeStatementFixer::class => null,
-        PhpdocAlignFixer::class => null,
-        PhpdocInlineTagFixer::class => null,
-        PhpdocNoAliasTagFixer::class => null,
-        PhpdocNoEmptyReturnFixer::class => null,
-        PhpdocNoPackageFixer::class => null,
-        PhpdocSeparationFixer::class => null,
-        PhpdocSummaryFixer::class => null,
-        PhpdocToCommentFixer::class => null,
-        ReturnAssignmentFixer::class => null,
-        StrictComparisonFixer::class => null,
-        StrictParamFixer::class => null,
-        BlankLineBeforeStatementFixer::class => null,
-        MethodChainingIndentationFixer::class => null,
-        NoExtraBlankLinesFixer::class => null,
-        SlamCsFixer\FinalInternalClassFixer::class => null,
-        TraitUseSpacingSniff::class => null,
-        SlevomatCodingStandard\Sniffs\Classes\UnusedPrivateElementsSniff::class => null,
-        EmptyCommentSniff::class => null,
-        ReferenceUsedNamesOnlySniff::class => null,
-        UselessVariableSniff::class => null,
-
-        ClassCommentSniff::class . '.Missing' => null,
-        FileCommentSniff::class . '.Missing' => null,
-        FileCommentSniff::class . '.WrongStyle' => null,
-
-        ValidClassNameSniff::class => ['**/whatwedo*.php'],
+    $ecsConfig->rule(ValidClassNameSniff::class);
+    $ecsConfig->rule(ClassCommentSniff::class);
+    $ecsConfig->rule(FileCommentSniff::class);
+    $ecsConfig->rule(FunctionCommentThrowTagSniff::class);
+    $ecsConfig->ruleWithConfiguration(OrderedClassElementsFixer::class, [
+        'order' => [
+            'use_trait',
+            'constant_public',
+            'constant_protected',
+            'constant_private',
+            'property_public',
+            'property_protected',
+            'property_private',
+            'construct',
+            'destruct',
+            'method_public',
+            'method_protected',
+            'method_private',
+            'phpunit',
+            'magic',
+        ],
     ]);
+    $ecsConfig->rule(NoImportFromGlobalNamespaceFixer::class);
+    $ecsConfig->rule(NoNullableBooleanTypeFixer::class);
+    $ecsConfig->rule(NoPhpStormGeneratedCommentFixer::class);
+    $ecsConfig->rule(PhpdocSelfAccessorFixer::class);
+    $ecsConfig->rule(PhpdocVarAnnotationCorrectOrderFixer::class);
+    $ecsConfig->rule(ForbiddenAnnotationsSniff::class);
+    $ecsConfig->rule(AssignmentInConditionSniff::class);
+    $ecsConfig->rule(DeadCatchSniff::class);
+    $ecsConfig->rule(UseFromSameNamespaceSniff::class);
+    $ecsConfig->rule(NoImportFromGlobalNamespaceFixer::class);
+    $ecsConfig->rule(NoImportFromGlobalNamespaceFixer::class);
+    $ecsConfig->rule(NoImportFromGlobalNamespaceFixer::class);
+    $ecsConfig->rule(NoImportFromGlobalNamespaceFixer::class);
+    $ecsConfig->rule(NoImportFromGlobalNamespaceFixer::class);
+
+    $ecsConfig->skip([
+            AssignmentInConditionSniff::class => null,
+            InlineControlStructureSniff::class => null,
+            ScopeIndentSniff::class => null,
+            ScopeClosingBraceSniff::class => null,
+            CastSpacesFixer::class => null,
+            NoTrailingCommaInListCallFixer::class => null,
+            YodaStyleFixer::class => null,
+            IsNullFixer::class => null,
+            BlankLineAfterNamespaceFixer::class => null,
+            BinaryOperatorSpacesFixer::class => null,
+            ConcatSpaceFixer::class => null,
+            IncrementStyleFixer::class => null,
+            NotOperatorWithSuccessorSpaceFixer::class => null,
+            UnaryOperatorSpacesFixer::class => null,
+            PhpCsFixer\Fixer\Phpdoc\BlankLineBeforeStatementFixer::class => null,
+            PhpdocAlignFixer::class => null,
+            PhpdocInlineTagFixer::class => null,
+            PhpdocNoAliasTagFixer::class => null,
+            PhpdocNoEmptyReturnFixer::class => null,
+            PhpdocNoPackageFixer::class => null,
+            PhpdocSeparationFixer::class => null,
+            PhpdocSummaryFixer::class => null,
+            PhpdocToCommentFixer::class => null,
+            ReturnAssignmentFixer::class => null,
+            StrictComparisonFixer::class => null,
+            StrictParamFixer::class => null,
+            BlankLineBeforeStatementFixer::class => null,
+            MethodChainingIndentationFixer::class => null,
+            NoExtraBlankLinesFixer::class => null,
+            SlamCsFixer\FinalInternalClassFixer::class => null,
+            TraitUseSpacingSniff::class => null,
+            SlevomatCodingStandard\Sniffs\Classes\UnusedPrivateElementsSniff::class => null,
+            EmptyCommentSniff::class => null,
+            ReferenceUsedNamesOnlySniff::class => null,
+            UselessVariableSniff::class => null,
+
+            ClassCommentSniff::class . '.Missing' => null,
+            FileCommentSniff::class . '.Missing' => null,
+            FileCommentSniff::class . '.WrongStyle' => null,
+            ValidClassNameSniff::class => ['**/whatwedo*.php'],
+    ]);
+
+    $ecsConfig->parallel();
 };
--- config/whatwedo-symfony.php
+++ config/whatwedo-symfony.php
@@ -2,17 +2,13 @@
 
 declare(strict_types=1);
 
-use PhpCsFixerCustomFixers\Fixer\NoDoctrineMigrationsGeneratedCommentFixer;
-use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
+use Symplify\EasyCodingStandard\Config\ECSConfig;
 use Symplify\EasyCodingStandard\ValueObject\Set\SetList;
+use PhpCsFixerCustomFixers\Fixer\NoDoctrineMigrationsGeneratedCommentFixer;
 
-return static function (ContainerConfigurator $containerConfigurator): void {
-    $services = $containerConfigurator->services();
-    $services->set(NoDoctrineMigrationsGeneratedCommentFixer::class);
+return static function (ECSConfig $ecsConfig): void {
+    $ecsConfig->sets([SetList::SYMFONY]);
+    $ecsConfig->import(__DIR__ . '/whatwedo-common.php');
 
-    $parameters = $containerConfigurator->parameters();
-
-    // Import needs to be at the end - SKIP isn't merging (https://github.com/symplify/symplify/issues/2906)
-    $containerConfigurator->import(SetList::SYMFONY);
-    $containerConfigurator->import(__DIR__ . '/whatwedo-common.php');
+    $ecsConfig->rule(NoDoctrineMigrationsGeneratedCommentFixer::class);
 };
