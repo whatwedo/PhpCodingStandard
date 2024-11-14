92c60c6f700bee092b193c2c596145032779916c
Maurizio Monticelli <maurizio@whatwedo.ch>
2024-11-14 17:37:17 +0100

    feat: add php cs fixer configuration for symfony

---
+++ src/PhpCsFixerConfig/SymfonyCsFixerConfig.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace whatwedo\PhpCodingStandard\PhpCsFixerConfig;
+
+use PhpCsFixer;
+
+class SymfonyCsFixerConfig
+{
+    public static function createFixer(string $projectDir): PhpCsFixer\ConfigInterface
+    {
+        $finder = new PhpCsFixer\Finder();
+        self::configureFinder($finder, $projectDir);
+
+        $config = new PhpCsFixer\Config();
+        $config->setFinder($finder);
+        self::configure($config);
+        return $config;
+    }
+
+
+    public static function configureFinder(PhpCsFixer\Finder $finder, string $projectDir)
+    {
+        $finder->in($projectDir)
+            ->exclude([
+                    'assets',
+                    'bin',
+                    'config',
+                    'node_modules',
+                    'public',
+                    'vendor',
+                    'var',
+                ]
+            );
+    }
+    public static function configure(PhpCsFixer\ConfigInterface $config)
+    {
+        $config->setRiskyAllowed(true)
+        ->setRules([
+            '@PER-CS' => true,
+            '@PHP83Migration' => true,
+            '@PSR12' => true,
+            '@Symfony' => true,
+            'strict_param' => true,
+            // make ecs compatible
+            'phpdoc_to_comment' => false,
+            'single_line_throw' => false,
+        ]);
+    }
+}
